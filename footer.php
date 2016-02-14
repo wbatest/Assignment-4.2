0034786679b7334d18f9cf33bbcb4d9b3ce81e26
Bates Tillman <w.bates.tillman@gmail.com>
2016-02-14 15:20:28 -0600

    Week 4 Assignment

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package batesT
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+			<h1>Contact Them</h1>
+			<p>I want to believe</p>
+			<p>Where are the lone gunmen?</p>
+			<p>Yes I have been watching the X Files recently</p>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
